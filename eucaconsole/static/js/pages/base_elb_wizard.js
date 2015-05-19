114df889bd8ad7050ce6bb13ec424db813f5cf05
Kyo Lee <kyolee310@gmail.com>
2015-05-19 14:21:12 -0700

    Added comments on the base elb wizard JS file

--- eucaconsole/static/js/pages/base_elb_wizard.js
+++ eucaconsole/static/js/pages/base_elb_wizard.js
@@ -23,27 +23,39 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
             $scope.setFocus();
         };
         $scope.setInitialValues = function (options) {
+            // retrieve the resource name for this wizard
             if (options.hasOwnProperty('resource_name')) {
                 $scope.resourceName = options.resource_name;
             }
+            // retrieve the navigation tab list for this wizard
             if (options.hasOwnProperty('wizard_tab_list')) {
                 $scope.tabList = options.wizard_tab_list;
             }
+            // retrieve the total navigation tab count for this page
             $scope.totalSteps = $scope.tabList.length;
+            // retrieve the form object using the resource name above
             $scope.thisForm = $('#' + $scope.resourceName + '-form');
             $scope.urlParams = $.url().param();
+            // set the current tab display index to be 0
             $scope.currentStepIndex = 0;
+            // set the default validation error to be true, thus disabling the next button by default
             $scope.isValidationError = true;
+            // create an array to maintain invalid step flags
+            // set all steps to be invalid at first, except the first tab page
             $scope.invalidSteps = Array.apply(undefined, Array($scope.totalSteps));
             angular.forEach($scope.invalidSteps, function(a, index){
                 $scope.invalidSteps[index] = true;
             });
             $scope.invalidSteps[0] = false;
+            // create an array to maintain each step's active class
+            // set the first tab to be 'active'
             $scope.stepClasss = Array.apply(undefined, Array($scope.totalSteps));
             angular.forEach($scope.stepClasses, function(a, index){
                 $scope.stepClasses[index] = '';
             });
             $scope.stepClasses[$scope.currentStepIndex] = 'active';
+            // create an array to maintain the summary panel display flags
+            // set the first section of the summary panel to be visible
             $scope.summaryDisplays = Array.apply(undefined, Array($scope.totalSteps));
             angular.forEach($scope.summaryDisplays, function(a, index){
                 $scope.summaryDisplays[index] = false;
@@ -53,20 +65,26 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
         $scope.setWatcher = function (){
             $scope.$watch('currentStepIndex', function(){
                 if( $scope.currentStepIndex !== 0 ){
+                    // adjust the focus on the wizard
                     $scope.setWizardFocus($scope.currentStepIndex);
                 }
+                // inform the child controllers about the navigation tab update
                 $scope.$broadcast('currentStepIndexUpdate', $scope.currentStepIndex);
             });
+            // handle the visit next step request from the child controllers
             $scope.$on('eventProcessVisitNextStep', function($event, nextStep) {
                 $scope.processVisitNextStep(nextStep);
             });
+            // handle the validation error flag set request from the child controllers
             $scope.$on('updateValidationErrorStatus', function($event, flag) {
                 $scope.isValidationError = flag;
             });
+            // handle the validation check request from the child controllers
             $scope.$on('requestValidationCheck', function($event) {
                 var currentStepID = $scope.currentStepIndex + 1;
                 $scope.existInvalidFields(currentStepID);
             });
+            // handle the opening of a modal on the wizard
             $(document).on('open.fndtn.reveal', '[data-reveal]', function () {
                 // When a dialog opens, reset the progress button status
                 $(this).find('.dialog-submit-button').css('display', 'block');
@@ -74,11 +92,13 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
                 // Broadcast initModal signal to trigger the modal initialization
                 $scope.$broadcast('initModal');
             });
+            // handle the submit event from a modal on the wizard
             $(document).on('submit', '[data-reveal] form', function () {
                 // When a dialog is submitted, display the progress button status
                 $(this).find('.dialog-submit-button').css('display', 'none');
                 $(this).find('.dialog-progress-display').css('display', 'block');
             });
+            // handle the closing of a modal on the wizard
             $(document).on('closed.fndtn.reveal', '[data-reveal]', function () {
                 var modal = $(this);
                 modal.find('input[type="text"]').val('');
@@ -94,6 +114,7 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
             });
         };
         $scope.setFocus = function () {
+            // adjust the focus to the opened modal
             $(document).on('opened.fndtn.reveal', '[data-reveal]', function () {
                 var modal = $(this);
                 var modalID = $(this).attr('id');
@@ -113,6 +134,7 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
                }
             });
         };
+        // set the focus to the current tab on the wizard
         $scope.setWizardFocus = function (stepIdx) {
             var tabElement = $(document).find('#tabStep'+(stepIdx+1)).get(0);
             if (!!tabElement) {
@@ -136,6 +158,7 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
             }
             return $scope.isValidationError;
         };
+        // handle the clicking of the next step visit link from the template
         $scope.visitStep = function($event, step) {
             $event.preventDefault();
             var nextStep = step;
@@ -143,8 +166,15 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
             if ($scope.tabList[step].render === false) {
                 nextStep = step + 1;
             }
+            // broadcast the vist next step event to the child controllers
+            // so that they can perform proper procedures per tab section
+            // inform the current step index and the intended next step
             $scope.$broadcast('eventClickVisitNextStep', $scope.currentStepIndex+1, nextStep);
         };
+        // handle the vist next step request
+        // after sending out the 'eventClickVisitNextStep',
+        // the child controllers would confirm the event by replying with 'eventProcessVisitNextStep',
+        // which triggers this function call
         $scope.processVisitNextStep = function(nextStep) {
             // Check for form validation before proceeding to next step
             var currentStepID = $scope.currentStepIndex + 1;
@@ -176,6 +206,8 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
                 });
             }
         };
+        // a method to trigger the tab update on the wizard,
+        // rather than relying the actual click event, which is processed by Foundaiton
         $scope.updateStep = function(step) {
             // Adjust the tab classes to match Foundation's display 
             $("#wizard-tabs").children("dd").each(function() {
@@ -206,6 +238,7 @@ angular.module('BaseELBWizard', ['EucaConsoleUtils', 'MagicSearch', 'TagEditor',
         $scope.showSummarySecton = function(step) {
             $scope.summaryDisplays[step] = true;
         };
+        // clear all 'error' classes set by Foundation's valdation
         $scope.clearErrors = function(step) {
             $('#step'+step).find('div.error').each(function(idx, val) {
                 $(val).removeClass('error');
